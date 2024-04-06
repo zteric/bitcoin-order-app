5895deba035b27a078503f1aa4ad4120eaa58699
Kaidi Zhang <kaidi.zhang1@singtel.com>
2024-04-06 15:04:06 +0800

    lint

--- scripts /jshint-script.sh
+++ scripts /jshint-script.sh
@@ -2,5 +2,4 @@
 
 jshint --exclude="node_modules/" --reporter=unix . > JSHint-report
 
-
 echo $? > /dev/null
--- scripts /zap-script.sh
+++ scripts /zap-script.sh
@@ -3,4 +3,5 @@
 docker pull owasp/zap2docker-stable
 docker run -i owasp/zap2docker-stable zap-baseline.py -t "https://kenken64.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
 
+
 echo $? > /dev/null
